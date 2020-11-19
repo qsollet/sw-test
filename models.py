444a2c3bb6d9bee507148c75ab1137aef0eb013f
Quentin Sollet <quentin@sollet.fr>
2020-11-19 17:11:35 +0100

    added function to auto generate id for shorturl

--- models.py
+++ models.py
@@ -1,7 +1,8 @@
 from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.sql import func
 from .database import Base
-import datetime
+import random
+import string
 
 class Shorturl(Base):
     __tablename__ = 'shorturls'
@@ -11,9 +12,18 @@ class Shorturl(Base):
     created_at = Column(DateTime, default=func.now())
     updated_at = Column(DateTime, onupdate=func.now())
 
-    def __init__(self, id=None, url=None):
+    def __init__(self, url, id=None):
+        # If no id is given then generate one that is not already set
+        if id is None:
+            while (id := self.generate_id()):
+                if Shorturl.query.filter(Shorturl.id == id).first() == None:
+                    break
         self.id = id
         self.url = url
 
     def __repr__(self):
         return '<Shorturl %s -- %s>' % (self.id, self.url)
+
+    def generate_id(self, length=6):
+        chars = string.ascii_letters + string.digits
+        return ''.join(random.sample(chars, length))
